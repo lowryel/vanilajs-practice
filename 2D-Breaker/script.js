5b4a00e6d9fff9bbe88fef8208eb75e1b0cdb069
lowryel <eugeneagbaglo@gmail.com>
2022-09-12 16:43:05 +0000

    a

---
+++ 2D-Breaker/script.js
@@ -0,0 +1,13 @@
+const grid=document.querySelector('.grid')
+
+
+
+
+function addBlock(){
+    const block=document.createElement('div');
+    block.classList.add('block');
+    block.style.left='100px';
+    block.style.bottom='50px';
+    grid.appendChild(block);
+}
+addBlock()
